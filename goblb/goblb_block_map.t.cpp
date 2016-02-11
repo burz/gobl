f1291339ad9e150e1c0f473e711a7748a2f47c56
Anthony Burzillo <burz@jhu.edu>
2016-02-10 21:19:39 -0500

    Removed unecessary validators

--- goblb/goblb_block_map.t.cpp
+++ goblb/goblb_block_map.t.cpp
@@ -1,9 +1,10 @@
 // goblb_block_map.t.cpp
 
 #include <goblb_block_map.h>
-#include <goblb_validators.t.h>
 
 #include <gtest/gtest.h>
+#include <map>
+#include <set>
 #include <sstream>
 
 namespace goblb {
@@ -66,6 +67,126 @@ void insertEmptyKoShape(
     blockMap.insert(block3);
     blockMap.insert(block4);
 }
+
+class BlockMapRemoveValidator
+{
+  public:
+    // TYPES
+    typedef std::pair<std::set<Space::Ptr>, unsigned int> SpaceSetLiberties;
+    typedef std::map<Block::Ptr, SpaceSetLiberties> LibertiesMap;
+
+  private:
+    // DATA
+    BlockMap& d_blockMap;
+
+    std::vector<Space::Ptr> d_spaces;
+    LibertiesMap            d_exterior;
+
+  public:
+    // CREATORS
+    BlockMapRemoveValidator(BlockMap& blockMap)
+    : d_blockMap(blockMap)
+    {
+    }
+
+  private:
+    // PRIVATE MANIPULATORS
+    void setupBlock(
+          Block::Ptr adjacentBlock_p
+        , const Space::Ptr& space_p
+    )
+    {
+        if(adjacentBlock_p)
+        {
+            SpaceState::Value adjacentState = adjacentBlock_p->state();
+
+            if(space_p->state() != adjacentState)
+            {
+                ASSERT_LT(0u, adjacentBlock_p->libs());
+
+                LibertiesMap::iterator pos =
+                    d_exterior.find(adjacentBlock_p);
+
+                if(d_exterior.end() != pos)
+                {
+                    pos->second.first.insert(space_p);
+                }
+                else
+                {
+                    d_exterior.insert(LibertiesMap::value_type(
+                          adjacentBlock_p
+                        , SpaceSetLiberties(
+                              std::set<Space::Ptr>({space_p})
+                            , adjacentBlock_p->libs()
+                          )
+                    ));
+                }
+            }
+        }
+    }
+
+    void setup(const Block::Ptr& block_p)
+    {
+        ASSERT_TRUE(static_cast<bool>(block_p));
+
+        for(auto itt = block_p->members().begin()
+          ; block_p->members().end() != itt
+          ; ++itt)
+        {
+            d_spaces.push_back(*itt);
+
+            setupBlock(
+                  d_blockMap.lookup((*itt)->i() + 1, (*itt)->j())
+                , *itt
+            );
+            setupBlock(
+                  d_blockMap.lookup((*itt)->i() - 1, (*itt)->j())
+                , *itt
+            );
+            setupBlock(
+                  d_blockMap.lookup((*itt)->i(), (*itt)->j() + 1)
+                , *itt
+            );
+            setupBlock(
+                  d_blockMap.lookup((*itt)->i(), (*itt)->j() - 1)
+                , *itt
+            );
+        }
+    }
+
+    void runValidation()
+    {
+        for(auto itt = d_spaces.begin(); itt != d_spaces.end(); ++itt)
+        {
+            EXPECT_EQ(SpaceState::EMPTY, (*itt)->state());
+        }
+
+        for(auto itt = d_exterior.begin(); itt != d_exterior.end(); ++itt)
+        {
+            const unsigned int membersTouching = itt->second.first.size();
+            const unsigned int blockLiberties = itt->second.second;
+
+            EXPECT_EQ(blockLiberties + membersTouching, itt->first->libs());
+        }
+    }
+
+  public:
+    // MANIPULATORS
+    void validate(const Block::Ptr& block_p)
+    {
+        setup(block_p);
+
+        std::cout << "!!! BEFORE:" << std::endl
+                  << d_blockMap << std::endl;
+
+        d_blockMap.remove(block_p);
+
+        std::cout << "!!! AFTER:" << std::endl
+                  << d_blockMap << std::endl;
+
+        runValidation();
+    }
+};
 
 } // Close anonymous
 
